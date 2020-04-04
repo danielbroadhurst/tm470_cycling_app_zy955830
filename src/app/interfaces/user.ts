f0db20fe5e76a1af89eb436dc392d76e84db1f18
Daniel Broadhurst <djdanielbroadhurst@gmail.com>
2020-04-04 16:24:37 +0100

    CREATED - User

--- src/app/interfaces/user.ts
+++ src/app/interfaces/user.ts
@@ -1,10 +1,13 @@
+import { Profile } from './profile';
+
 export class User {
 
     constructor(
+      public id: number,
       public firstName: string,
       public lastName: string,
       public email: string,
-      public password: string,
+      public userProfile: Profile[],
     ) {  }
   
   }
