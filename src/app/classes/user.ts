80011eb22fd4412db48887dbc1d2ab0da8e67b65
Daniel Broadhurst <djdanielbroadhurst@gmail.com>
2020-04-05 20:09:53 +0100

    UPDATED - User Class

--- src/app/classes/user.ts
+++ src/app/classes/user.ts
@@ -7,7 +7,7 @@ export class User {
       public first_name: string,
       public last_name: string,
       public email: string,
-      public user_profile: Profile[],
+      public user_profile: Profile,
     ) {  }
   
   }
